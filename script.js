66719a41d98a58173dd794124dcdfee393a91c1b
camposjosh118-sys <camposjosh118@gmail.com>
2026-07-27 00:17:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function abrirCarta() {
-    const tapa = document.querySelector(".tapa");
-    const contenido = document.querySelector(".contenido");
+function comprobarClave() {
 
-    tapa.style.transform = "scale(0)";
+    const clave = document.getElementById("clave").value;
+    const mensaje = document.getElementById("mensaje");
 
-    setTimeout(() => {
-        tapa.style.display = "none";
-        contenido.style.display = "block";
-    }, 800);
+    if (clave.toLowerCase() === "flan🍮") {
+
+        document.getElementById("inicio").style.display = "none";
+
+        document.getElementById("carta").style.display = "block";
+
+    } else {
+
+        mensaje.innerHTML = "Ese no es el apodo que me dices de cariño 🍮❤️";
+
+    }
+
+}
+
+
+function abrirMusica() {
+
+    window.open(
+        "https://www.youtube.com/results?search_query=Santa+Fe+Klan+Maldita+Mujer",
+        "_blank"
+    );
+
 }
